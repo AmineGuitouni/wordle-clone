90a48a84e339654a357dbb944a4abee28e67c03f
AmineGuitouni <Notam1ne2002@gmail.com>
2024-06-17 08:50:50 +0100

    fix keyboard

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -8,16 +8,17 @@ export const CheckWord = async ({word, encryptedWord}:{word: string, encryptedWo
         throw new Error('Word must be 5 characters long')
     }
 
-    let randomWord = decrypt(encryptedWord, process.env.ENCREPTION_KEY!, process.env.ENCREPTION_IV!)
-    console.log(!WordsList.includes(randomWord.toLowerCase()))
+    const decreptedWord = decrypt(encryptedWord, process.env.ENCREPTION_KEY!, process.env.ENCREPTION_IV!)
+    let randomWord = decreptedWord
+    
     if(!WordsList.includes(randomWord.toLowerCase())){
         throw new Error('Word not found')
     }
+    
     const win = randomWord === word
     let colors = ['R', 'R', 'R', 'R', 'R']
     let letters:{letter: string, type: "right" | "wrong" | "missPlaced"}[] = []
 
-    console.log("1")
     for (let i = 0; i < 5; i++) {
         if (word[i] === randomWord[i]) {
             colors[i] = 'G'
@@ -25,19 +26,17 @@ export const CheckWord = async ({word, encryptedWord}:{word: string, encryptedWo
             letters.push({letter: word[i], type: 'right'})
         }
     }
-    console.log("2")
+
     for (let i = 0; i < 5; i++) {
         if (randomWord.includes(word[i])) {
             colors[i] = 'Y'
             randomWord = randomWord.replace(word[i], '_')
             letters.push({letter: word[i], type: 'missPlaced'})
         }
-        else if(word[i] !== '_' && colors[i] !== 'G') {
+        else if(word[i] !== '_' && !decreptedWord.includes(word[i])) {
             letters.push({letter: word[i], type: 'wrong'})
         }
     }
-
-
 
     return {colors, win, letters}
 }
